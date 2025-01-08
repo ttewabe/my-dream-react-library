fe367d792fa241a74f41c55ff8f8d5b7beb534d2
Ethiopian Community <tomtadege@gmail.com>
2025-01-07 22:40:00 -0800

    added Card component

---
+++ src/components/Card.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const CardStyles = styled.div`
+background-color: #ddefef;
+  padding: 10px;
+  border: 2px solid lightgray;
+  border-radius:15px;
+  width: 400px;
+  margin: 10px;
+  box-shadow: 5px 5px 5px lightgray;
+  transition: all 0.9s ease;
+  `;
