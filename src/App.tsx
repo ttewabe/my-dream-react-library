0fc361552bb150f1580761c0a73266a1e7f42298
Ethiopian Community <tomtadege@gmail.com>
2025-01-11 20:52:28 -0800

    update app and main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { MultiSelect } from './tat-react-project/multi-select/MultiSelect';
 
-const App = () => {
+export const App = () => {
     return (
         <div>
             <MultiSelect label={''} placeholder={''} />
         </div>
     );
 };
-export default App;
